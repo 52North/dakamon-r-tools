391b001a8b9fb951cd35961529a7d09ebcde7a77
EHJ-52n <e.h.juerrens@52north.org>
2018-08-22 14:31:49 +0200

    Fixed whitespace errors

--- DaKaMon_importer/server_Ort.R
+++ DaKaMon_importer/server_Ort.R
@@ -13,27 +13,27 @@ decOrt <- decSep
 observeEvent(input$csvFileOrt, {
   valiOrt$validated <- FALSE
   checkDBOrt$checked <- FALSE
-  
+
   # check whether an encoding has been set; fallback: guess the eoncoding using readr
   if (is.null(csvEncode)) {
     csvEncode <- readr::guess_encoding(input$csvFileOrt$datapath)
     csvEncode <- csvEncode$encoding[which.max(csvEncode$confidence)]
   }
-  
+
   inCSVOrt$csvEncode <- csvEncode
-  
+
   inCSVOrt$df <- read.csv(input$csvFileOrt$datapath,
                           header = TRUE,
                           sep = sepOrt, dec = decOrt,
-                          stringsAsFactors = FALSE, 
+                          stringsAsFactors = FALSE,
                           fileEncoding = inCSVOrt$csvEncode)
-  
+
   inCSVOrt$headAsChar <- colnames(inCSVOrt$df)
-  
-  ## validation of Ort csv-file 
+
+  ## validation of Ort csv-file
   # look for required column names
   # check whether columns have unique names
-  
+
   txt <- NULL
   if (!(reqColOrt$id %in% inCSVOrt$headAsChar) || length(unique(inCSVOrt$df[,reqColOrt$id])) != length(inCSVOrt$df[,reqColOrt$id]))
     txt <- paste0(txt, "<li>Jeder Ort benötigt eine persistente und eindeutige ID in der Spalte'", reqColOrt$id, "'.</li>")
@@ -41,16 +41,15 @@ observeEvent(input$csvFileOrt, {
     if (!(reqColName %in% inCSVOrt$headAsChar))
       txt <- paste0(txt, "<li>Bitte die Spalte '", reqColName, "' ergänzen.</li>", sep="")
   }
-  
+
   if(length(unique(inCSVOrt$headAsChar)) != length(inCSVOrt$headAsChar))
     txt <- paste0(txt, "<li>Bitte nur eindeutige Spaltennamen verwenden.</li>")
-  
+
   valiOrt$txt <- txt
   valiOrt$validated <- TRUE
 })
 
 # write txt feedback as html list - or action button
-
 output$OrtValidationOut <- renderUI({
   if (valiOrt$validated) {
     if (is.null(valiOrt$txt)) {
@@ -72,12 +71,12 @@ output$OrtValidationOut <- renderUI({
 observeEvent(input$checkDBOrt, {
   db <- dbConnect("PostgreSQL", host=dbHost, dbname=dbName, user=dbUser, password=dbPassword, port=dbPort)
   on.exit(dbDisconnect(db), add=T)
-  
+
   progress <- shiny::Progress$new()
   on.exit(progress$close(), add = T)
-  
+
   progress$set(message = "Prüfe bereits registrierte Orte.", value = 0)
-  
+
   # get all Orte from the DB that have any of the identifiers in the CSV
   OrtInDB <- dbGetQuery(db, paste0("SELECT featureofinterestid, identifier FROM featureofinterest WHERE identifier IN ('",
                                    paste(inCSVOrt$df[,reqColOrt$id], collapse="', '"),"')"))
@@ -87,14 +86,15 @@ observeEvent(input$checkDBOrt, {
   } else {
     checkDBOrt$txt <- NULL
   }
-  
+
   checkDBOrt$OrtInDB <- OrtInDB
-  
+
   checkDBOrt$checked <- TRUE
 }, ignoreInit=TRUE)
 
 
 # output of DB consistency check as html - or action button
+
 output$OrtDBConsistencyOut <- renderUI({
   if (checkDBOrt$checked) {
     if (is.null(checkDBOrt$txt)) {
@@ -107,25 +107,27 @@ output$OrtDBConsistencyOut <- renderUI({
   }
 })
 
+
 # plot table with CSV
+
 output$tableOrt <- renderDataTable({
   if (!is.null(inCSVOrt$df)) {
     showTab <- inCSVOrt$df
-    
+
     showHead <- paste0("<span style=\"white-space: nowrap; display: inline-block; text-align: left\">", colnames(showTab))
-    
+
     showHead <- paste0(showHead, "</span>")
-    
+
     showDT <- datatable(showTab, colnames = showHead,
                         options = list(paging=FALSE, bFilter=FALSE,
                                        scrollX=TRUE, sort=FALSE, dom="t",
                                        language=list(url = lngJSON)),
                         escape=FALSE)
-    
+
     # if DB consistency has been checked, apply colors
     if (checkDBOrt$checked) {
       rowColors <- rep("white", nrow(showTab))
-      
+
       if (nrow(checkDBOrt$OrtInDB) > 0) {
         rowColors[showTab$ID %in% checkDBOrt$OrtInDB] <- "red"
         showDT <- formatStyle(showDT, "ID", target="row",
@@ -144,28 +146,28 @@ observeEvent(input$storeDBOrt, {
   #db <- dbConnect("PostgreSQL", host=dbHost, dbname=dbName, user=dbUser, password=dbPassword, port=dbPort)
   db <- connectToDB()
   on.exit(dbDisconnect(db), add=T)
-  
+
   Ort_data <- inCSVOrt$df
   Ort_header <- inCSVOrt$headAsChar
-  
+
   Ort_empty_cols <- apply(Ort_data, 2, function(x) all(is.na(x)))
-  
+
   Ort_header <- Ort_header[!Ort_empty_cols]
   Ort_data <- Ort_data[,!Ort_empty_cols]
-  
+
   nRowDf <- nrow(Ort_data)
-  
+
   progress <- shiny::Progress$new()
   on.exit(progress$close(), add=T)
-  
+
   progress$set(message = "Füge Orte in DB ein.", value = 0)
-  
+
   ## add missign columns
   regCols <- dbGetQuery(db, paste0("SELECT dede FROM column_metadata WHERE prefixid IN ('ort', 'global')"))[,1]
   ortDataCols <- dbGetQuery(db, paste0("SELECT columnid, prefixid, dede FROM column_metadata WHERE prefixid IN ('ort')"))
   misCols <- which(sapply(Ort_header, # TODO drop ID and Name
                           function(x) is.na(match(x, regCols))))
-  
+
   if (length(misCols > 0)) {
     for (i in 1:length(misCols)) {# i <- 1
       colId <- sprintf("col%03d", i + length(regCols))
@@ -173,10 +175,10 @@ observeEvent(input$storeDBOrt, {
                        integer = "numeric",
                        numeric = "numeric",
                        character = "character varying(255)")
-      
+
       # TODO adopt to new FoI table
       dbSendQuery(db, paste0("ALTER TABLE ort_data ADD COLUMN ", colId, " ", coltype, ";"))
-      
+
       dbSendQuery(db, paste0("INSERT INTO column_metadata (columnid, prefixid, dede)
                                VALUES ('", paste(colId, 'ort', Ort_header[misCols[i]], sep="', '"),"')"))
     }
@@ -224,12 +226,12 @@ observeEvent(input$storeDBOrt, {
           }
         } else {
           if (class(value) == "character") {
-            value = paste0("'", value, "'")       
+            value = paste0("'", value, "'")
           }
           dynamicValues = paste(dynamicValues, value, sep=', ') 
         }
       }
-      
+
       insertFeature = paste("INSERT INTO featureofinterest (featureofinterestid, featureofinteresttypeid, identifier, name, geom)
                              VALUES (nextval('featureofinterestid_seq'), 1",
                             paste0("'", Ort_data[ort,reqColOrt$id], "'"),
@@ -244,7 +246,8 @@ observeEvent(input$storeDBOrt, {
       dbSendQuery(db, query)
     }
   }
-  
+
+
   showModal(modalDialog(
     title = "Vorgang abgeschlossen.",
     "Orte erfolgreich angelegt.",
