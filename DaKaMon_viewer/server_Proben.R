3da8877c69c212c2dca86b20eabe2838fb9f8a04
Carsten Hollmann <c.hollmann@52north.org>
2018-08-22 16:00:38 +0200

    works with PNS identifier instead of name, with name the filtering does not work but if you execute the query in pgAdmin the expected result is returned.

--- DaKaMon_viewer/server_Proben.R
+++ DaKaMon_viewer/server_Proben.R
@@ -1,54 +1,70 @@
 ## server Proben-Ansicht 
 
-probenPNS <- reactive({
-  db <- connectToDB()
-  
-  # Alle PNS/Global Spaltennamen
-  # pnsDataMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('pns', 'global')"))
-  
-  # Nur PNS Spaltename
-  pnsDataPnsMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('pns')"))
-  
-  # Add Prefix zu Spaltennamen
-  pnsColColumns <- paste0("pns.", grep("col*", pnsDataPnsMetaData$columnid, value = TRUE))
-  
-  # Query alle PNS TODO: Namen genügen
-  res <- dbGetQuery(db, paste0("SELECT foi.featureofinterestid, foi.identifier, foi.name, pfoi.identifier as orts_id, ", paste0(pnsColColumns, collapse=", "),
-                        " FROM featureofinterest foi
-                      RIGHT OUTER JOIN pns_data pns ON foi.featureofinterestid = pns.featureofinterestid
-                      RIGHT OUTER JOIN featurerelation fr ON foi.featureofinterestid = fr.childfeatureid
-                      LEFT OUTER JOIN featureofinterest pfoi ON pfoi.featureofinterestid = fr.parentfeatureid"))
-  dbDisconnect(db)
-  
-  res
-})
-
-output$probenPNSInput <- renderUI(selectInput("probenPNS", "Probenahmestelle:", 
-                                              probenPNS()$name, multiple = TRUE, 
-                                              selected = probenPNS()$name[1]))
-
-
-# Alle Proben/Global Spaltennamen
-# paramDataMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('param', 'global')"))
 
 db <- connectToDB()
 
-# Alle Probe/Global Spaltennamen
-# probeDataMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('probe', 'global')"))
+# Alle PNS/Global Spaltennamen
+# pnsDataMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('pns', 'global')"))
 
-# Nur Probe Spaltename
-probeDataProbeMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('probe')"))
+# Nur PNS Spaltename
+pnsDataPnsMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('pns')"))
 
 # Add Prefix zu Spaltennamen
-probeColColumns <- paste0("pro.", grep("col*", probeDataProbeMetaData$columnid, value = TRUE))
+pnsColColumns <- paste0("pns.", grep("col*", pnsDataPnsMetaData$columnid, value = TRUE))
 
-# Query alle Proben mit PNS-Identifier TODO: Filter PNS aus input$probenPNS
-allProben <- dbGetQuery(db, paste0("SELECT pro.id, pro.identifier, pns.identifier as pns_id, ", paste0(probeColColumns, collapse=", "), 
-                                   "FROM probe pro
-                                    LEFT OUTER JOIN featureofinterest pns ON pns.featureofinterestid = pro.pns_id"))
+# Query alle PNS TODO: Namen genügen
+probenPNS <- dbGetQuery(db, paste0("SELECT foi.featureofinterestid, foi.identifier, foi.name, pfoi.identifier as orts_id, ", paste0(pnsColColumns, collapse=", "),
+                      " FROM featureofinterest foi
+                    RIGHT OUTER JOIN pns_data pns ON foi.featureofinterestid = pns.featureofinterestid
+                    RIGHT OUTER JOIN featurerelation fr ON foi.featureofinterestid = fr.childfeatureid
+                    LEFT OUTER JOIN featureofinterest pfoi ON pfoi.featureofinterestid = fr.parentfeatureid"))
+dbDisconnect(db)
 
-output$tableProben  <- renderDT(datatable(allProben,
-                                          filter="top",
-                                          options = list(paging=FALSE, dom = 'Brt',
-                                                         language=list(url = lngJSON)),
-                                          escape=FALSE))
+
+output$probenPNSInput <- renderUI(selectInput("probenPNS", "Probenahmestelle:", 
+                                              probenPNS$identifier, multiple = TRUE, 
+                                              selected = probenPNS$identifier[1]))
+
+
+# Alle Proben/Global Spaltennamen
+allProben<- reactive({
+  db <- connectToDB()
+  
+  # Alle Probe/Global Spaltennamen
+  probeDataMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('probe', 'global')"))
+  
+  # Nur Probe Spaltename
+  probeDataProbeMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('probe')"))
+  
+  # Add Prefix zu Spaltennamen
+  probeColColumns <- paste0("pro.", grep("col*", probeDataProbeMetaData$columnid, value = TRUE))
+  
+  # Query alle Proben mit PNS-Identifier TODO: Filter PNS aus input$probenPNS
+  query <-  paste0("SELECT pro.id, pro.identifier, pns.identifier as pns_id, ", paste0(probeColColumns, collapse=", "), 
+                   " FROM probe pro 
+                   LEFT OUTER JOIN featureofinterest pns ON pns.featureofinterestid = pro.pns_id")
+  if (!is.null(input$probenPNS))
+    query <- paste0(query, " WHERE pns.identifier IN (", paste0("'", input$probenPNS, "'" ,collapse=", ") ,")")
+    
+  allPro <- dbGetQuery(db, query)
+
+  if (nrow(allPro) > 0)
+    colnames(allPro) <- probeDataMetaData$dede[match(colnames(allPro), probeDataMetaData$columnid)]
+  
+  dbDisconnect(db)
+
+  allPro
+})
+
+output$tableProben  <- renderDT({
+  showTab <- allProben()[,-1]
+  
+  showHead <- paste0("<span style=\"white-space: nowrap; display: inline-block; text-align: left\">", colnames(showTab))
+  
+  showHead <- paste0(showHead, "</span>")
+  
+  datatable(showTab,
+                        filter="top",
+                        options = list(paging=FALSE, dom = 'Brt',
+                                       language=list(url = lngJSON)),
+                        escape=FALSE)})
