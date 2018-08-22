05ca031a42965d1104d0dca3d224c84a62d22c88
Carsten Hollmann <c.hollmann@52north.org>
2018-08-22 15:59:38 +0200

    works

--- DaKaMon_viewer/server_Parameter.R
+++ DaKaMon_viewer/server_Parameter.R
@@ -1,40 +1,60 @@
 ## server Parameteransicht
-paramElemGroup <- reactive({
-  db <- connectToDB()
-  
-  col <- dbGetQuery(db, "SELECT columnid FROM column_metadata WHERE prefixid = 'param' AND dede = 'Stoffgruppe' limit 1")
-  res <- dbGetQuery(db, paste0("SELECT DISTINCT ", col, " as name FROM parameter_data
-                               WHERE ", col, " IS NOT NULL"))
-  dbDisconnect(db)
-  
-  res
-})
 
-output$paramElemGroupInput <- renderUI(selectInput("paramElemGroup", "Stoffgruppenauswahl:", 
-                                                   paramElemGroup()$name, multiple = TRUE, 
-                                                   selected = paramElemGroup()$name[1]))
-
-# Alle Parameter/Global Spaltennamen
-# paramDataMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('param', 'global')"))
 
 db <- connectToDB()
 
-# Nur Parameter Spaltename
-  paramDataParamMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('param')"))
-
-# Add Prefix zu Spaltennamen
-  paramColColumns <- paste0("param.", grep("col*", paramDataParamMetaData$columnid, value = TRUE))
-
-
-# Query alle Parameter: TODO: filter auf Stoffgruppe aus input$paramElemGroup
-  allParameter <- dbGetQuery(db, paste0("SELECT op.observablepropertyid, op.identifier, op.name, ", paste0(paramColColumns, collapse=", "),
-                                        " FROM observableproperty op
-                                    RIGHT OUTER JOIN parameter_data param ON op.observablepropertyid = param.observablepropertyid"))
+col <- dbGetQuery(db, "SELECT columnid FROM column_metadata WHERE prefixid = 'param' AND dede = 'Stoffgruppe' limit 1")
+res <- dbGetQuery(db, paste0("SELECT DISTINCT ", col, " as name FROM parameter_data
+                             WHERE ", col, " IS NOT NULL"))
+dbDisconnect(db)
 
 
-output$tableParameter  <- renderDT(datatable(allParameter,
-                                             filter="top",
-                                             options = list(paging=FALSE, dom = 'Brt',
-                                                            language=list(url = lngJSON)),
-                                             escape=FALSE))
+output$paramElemGroupInput <- renderUI(selectInput("paramElemGroup", "Stoffgruppenauswahl:", 
+                                                   res$name, multiple = TRUE, 
+                                                   selected = res$name[1]))
 
+
+allParameter<- reactive({
+    db <- connectToDB()
+    
+    # Alle Parameter/Global Spaltennamen
+    paramDataMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('param', 'global')"))
+    
+    # Nur Parameter Spaltename
+    paramDataParamMetaData <- dbGetQuery(db, paste0("SELECT * FROM column_metadata WHERE prefixid IN ('param')"))
+    
+    # Add Prefix zu Spaltennamen
+    paramColColumns <- paste0("param.", grep("col*", paramDataParamMetaData$columnid, value = TRUE))
+    
+    
+    # Query alle Parameter:
+    query <- paste0("SELECT op.observablepropertyid, op.identifier, op.name, ", paste0(paramColColumns, collapse=", "),
+                       " FROM observableproperty op
+                                            RIGHT OUTER JOIN parameter_data param ON op.observablepropertyid = param.observablepropertyid")
+
+    if (!is.null(input$paramElemGroup))
+      query <- paste0(query, " WHERE param.", col, " IN (", paste0("'", input$paramElemGroup, "'" ,collapse=", ") ,")")
+    
+    allParameter <- dbGetQuery(db, query)
+    
+    if (nrow(allParameter) > 0)
+      colnames(allParameter) <- paramDataMetaData$dede[match(colnames(allParameter), paramDataMetaData$columnid)]
+    
+    dbDisconnect(db)
+    allParameter
+})
+
+
+
+output$tableParameter  <- renderDT({
+    showTab <- allParameter()[,-1]
+    
+    showHead <- paste0("<span style=\"white-space: nowrap; display: inline-block; text-align: left\">", colnames(showTab))
+    
+    showHead <- paste0(showHead, "</span>")
+    
+    datatable(showTab,
+                                   filter="top",
+                                   options = list(paging=FALSE, dom = 'Brt',
+                                                  language=list(url = lngJSON)),
+                                   escape=FALSE)})
