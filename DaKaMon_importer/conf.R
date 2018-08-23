2490a6fd9bf95cec035c2cd61429db8c35c3db5f
EHJ-52n <e.h.juerrens@52north.org>
2018-08-23 12:22:53 +0200

    Fixed whitespace errors and added comments to conf.R

--- DaKaMon_importer/conf.R
+++ DaKaMon_importer/conf.R
@@ -1,6 +1,9 @@
 ## DaKaMon conf
+# common constants
 
-# common constants:
+#
+# CSV encoding
+#
 # column Separator
 colSep <- ";"
 # decimal separator
@@ -8,13 +11,21 @@ decSep <- ","
 # encoding
 csvEncode <- "UTF-8"
 
-# langiage
+
+#
+# I18N
+#
+# language
 lngJSON <- "http://cdn.datatables.net/plug-ins/1.10.11/i18n/German.json"
+
 
 csvInfo <- paste0("Die CSV-Datei muss \"", colSep, "\" als Spaltentrennzeichen und \"", decSep, "\" als Dezimaltrennzeichen verwenden und in \"", csvEncode, "\" enkodiert sein.")
 
 SOSWebApp <- "http://localhost:8080/52n-sos-webapp/"
+
+# FIXME verbose <- local (below) vs. verbose <- TRUE !?
 verbose <- TRUE
+
 BGencode <- 0
 BGchar <- "BG"
 BGlabel <- "Bestimmungsgrenze" # label in DB
@@ -25,19 +36,29 @@ NGlabel <- "Nachweisgrenze" # label in DB
 
 local <- interactive()
 SOSWebApp <- ifelse(local, "http://localhost:8080/52n-sos-webapp/", "http://sos:8080/52n-sos-webapp/")
+
+#
+# DATABASE
+#
 dbHost <- ifelse(local, "localhost", "db") 
 dbPort <- "5432"
 dbUser <- "postgres"
 dbPassword <- "postgres"
 dbName <- "sos"
+
+# FIXME verbose <- local vs. verbose <- TRUE (above) !?
 verbose <- local
 
+#
+# FEEDER
+#
 feederPath <- ifelse(local, "~/GitRepos/sos-importer/feeder/target/52n-sos-importer-feeder-bin.jar", "/usr/local/52n/52n-sos-importer-feeder-bin.jar")
 #
 # specifiies the number of parallel performed imports during measurement upload
 #
 feedNumberOfParallelImports <- 1
 #
+
 stndTime <- "T12:00:00+00:00"
 adminPwd <- "p"
 ifelse(local, 
